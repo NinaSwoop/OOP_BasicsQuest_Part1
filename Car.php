08f3134c4874ba9f18b9d10c398725bde3fe1427
Nina Iacoponelli <nina.iacoponelli@gmail.com>
2022-10-10 10:59:32 +0200

    Exercice done before challenge

--- Car.php
+++ Car.php
@@ -1,68 +1,44 @@
 <?php
 
-class Car {
-    private int $nbWheels;
+require_once 'Vehicle.php';
 
-    private int $currentSpeed;
-
-    private string $color;
-
-    private int $nbSeats;
+class Car extends Vehicle
+{
+    public const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+    ];
 
     private string $energy;
 
     private int $energyLevel;
 
     public function __construct(string $color, int $nbSeats, string $energy)
-{
-    $this->color = $color;
-}
-
-public function forward(): string
-{
-    $this->currentSpeed = 15;
-
-    return "Go !";
-}
-public function brake(): string
-{
-   $sentence = "";
-   while ($this->currentSpeed > 0) {
-       $this->currentSpeed--;
-       $sentence .= "Brake !!!";
-   }
-   $sentence .= "I'm stopped !";
-   return $sentence;
-}
-public function getNbWheels(): int
-{
-    return $this->nbWheels;
-}
-public function setCurrentSpeed(int $currentSpeed): void
-{
-    if($currentSpeed >= 0) {
-        $this->currentSpeed = $currentSpeed;
+    {
+        parent::__construct($color, $nbSeats);
+        $this->energy = $energy;
     }
-}
-public function getColor(): string
-{
-    return $this->color;
-}
-public function getNbSeats(): string
-{
-    return $this->nbSeats;
-}
-public function getEnergy(): string
-{
-    return $this->energy;
-}
-public function getEnergyLevel(): string
-{
-    return $this->energyLevel;
-}
-public function getCurrentSpeed(): int
-{
-return $this->currentSpeed;
-}
+    
 
+    public function getEnergy(): string
+    {
+        return $this->energy;
+    }
+
+public function setEnergy(string $energy): Car
+{
+    if (in_array($energy, self::ALLOWED_ENERGIES)) {
+        $this->energy = $energy;
+    }
+    return $this;
+}
+    public function getEnergyLevel(): int
+    {
+        return $this->energyLevel;
+    }
+
+    public function setEnergyLevel(int $energyLevel): void
+    {
+        $this->energyLevel = $energyLevel;
+    }
 }
--- index.php
+++ index.php
@@ -4,39 +4,49 @@ require_once'Bicycle.php';
 require_once'Car.php';
 
 
-$bike = new Bicycle('yellow');
-var_dump($bike);
+// $bike = new Bicycle('yellow');
+// var_dump($bike);
 
 // $bike->color = 'blue';
 // $bike->currentSpeed = 0;
 // var_dump($bike);
 
-var_dump($bike);
-$bike->dump();
+// var_dump($bike);
+// $bike->dump();
 
 // Moving bike
-echo $bike->forward();
-echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed() . ' km/h' . '<br>';
-echo $bike->brake();
-echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed(). ' km/h' . '<br>';
-echo $bike->brake();
+// echo $bike->forward();
+// echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed() . ' km/h' . '<br>';
+// echo $bike->brake();
+// echo '<br> Vitesse du vélo : ' . $bike->getCurrentSpeed(). ' km/h' . '<br>';
+// echo $bike->brake();
 
-$rockrider = new Bicycle('black');
+// $rockrider = new Bicycle('black');
 
 
 // Instanciation d'un nouvel objet $tornado
-$tornado = new Bicycle('pink');
+// $tornado = new Bicycle('pink');
 
-$tornado->forward();
+// $tornado->forward();
+
+$bicycle = new Bicycle('blue', 1);
+echo $bicycle->forward();
+var_dump($bicycle);
+
+$car = new Car('green', 4, 'electric');
+echo $car->forward();
+var_dump($car);
+
+var_dump(Car::ALLOWED_ENERGIES);
 
 // $car est une instance de Car. C'est l'instance que l'on dump
-$car = new Car('blue', 2, 'fuel');
-var_dump($car);
+// $car = new Car('blue', 2, 'fuel');
+// var_dump($car);
 
 
 // Moving car
-echo $car->forward();
-echo '<br> Vitesse du vélo : ' . $car->getCurrentSpeed() . ' km/h' . '<br>';
-echo $car->brake();
-echo '<br> Vitesse du vélo : ' . $car->getCurrentSpeed(). ' km/h' . '<br>';
-echo $car->brake();
+// echo $car->forward();
+// echo '<br> Vitesse du vélo : ' . $car->getCurrentSpeed() . ' km/h' . '<br>';
+// echo $car->brake();
+// echo '<br> Vitesse du vélo : ' . $car->getCurrentSpeed(). ' km/h' . '<br>';
+// echo $car->brake();
